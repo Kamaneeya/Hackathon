3fe4a618f531c8f44b1e5bd0f20c027123dead80
shivam <084ssharan@gmail.com>
2023-08-02 09:51:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-
-import "./App.css"
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import React, { useState } from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import useClipboard from "react-use-clipboard";
-import {useState} from "react";
-
 
 const App = () => {
-    const [textToCopy, setTextToCopy] = useState();
-    const [isCopied, setCopied] = useClipboard(textToCopy, {
-        successDuration:1000
-    });
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
 
-    //subscribe to thapa technical for more awesome videos
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
 
-    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
-    if (!browserSupportsSpeechRecognition) {
-        return null
-    }
+  return (
+    <>
+      <div className="container">
+        <h2>Hands Free Editor</h2>
+        <br />
+        <p>
+          An Advanced Editor that converts speech from the microphone to text
+          and makes it available to meaningful sentences.
+        </p>
 
-    return (
-        <>
-            <div className="container">
-                <h2>Speech to Text Converter</h2>
-                <br/>
-                <p>A React hook that converts speech from the microphone to text and makes it available to your React
-                    components.</p>
+        <textarea
+          className="main-content"
+          value={transcript}
+          onClick={() => setTextToCopy(transcript)}></textarea>
 
-                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
-                    {transcript}
-                </div>
-
-                <div className="btn-style">
-
-                    <button onClick={setCopied}>
-                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
-                    </button>
-                    <button onClick={startListening}>Start Listening</button>
-                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
-
-                </div>
-
-            </div>
-
-        </>
-    );
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={() => resetTranscript()}>Reset</button>
+          {/* Use the resetTranscript function from useSpeechRecognition */}
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
